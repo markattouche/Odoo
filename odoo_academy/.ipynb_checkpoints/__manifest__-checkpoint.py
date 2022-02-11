cc91b6590ae479d1deb165f62e6a4aa74e1130ba
Adonis A. Veloz <a.veloz@minicode.com.do>
2022-02-11 19:56:20 +0000

    Dia 3: Seguridad y vista basica

--- odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -2,21 +2,32 @@
 
 {
     'name': 'Odoo Academy',
+    
     'summary': """App académica de Entrenamiento""",
+    
     'description': """
         Módulo Académico de manejo de Entrenamiento:
             - Cursos
             - Sesiones
             - Participantes
     """,
+    
     'author': 'Minicode',
+    
     'website': 'https://minicode.odoo.com',
+    
     'category': 'Training',
+    
     'version': '0.1',
+    
     'depends': ['base'],
+    
     'data': [
-        
+        'security/academy_security.xml',
+        'security/ir.model.access.csv',
+        'views/academy_menuitems.xml'
     ],
+    
     'demo': [
         'demo/academy_demo.xml',
     ]
