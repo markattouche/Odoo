4b86dde6696a08a182405260544de5c6199ffa5e
Adonis Veloz <a.veloz@anonymoussoft.onmicrosoft.com>
2022-02-14 08:52:51 -0400

    Adding .gityignored and project cleaning

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,34 @@
+# .*. coding: utf-8 .*.
+
+{
+    'name': 'Odoo Academy',
+    
+    'summary': """App académica de Entrenamiento""",
+    
+    'description': """
+        Módulo Académico de manejo de Entrenamiento:
+            - Cursos
+            - Sesiones
+            - Participantes
+    """,
+    
+    'author': 'Minicode',
+    
+    'website': 'https://minicode.odoo.com',
+    
+    'category': 'Training',
+    
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        'security/academy_security.xml',
+        'security/ir.model.access.csv',
+        'views/academy_menuitems.xml'
+    ],
+    
+    'demo': [
+        'demo/academy_demo.xml'
+    ]
+}
