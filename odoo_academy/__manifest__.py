8b092360baf9118a40a17ac774e9368248933c28
Adonis A. Veloz <a.veloz@anonymoussoft.onmicrosoft.com>
2022-02-15 14:44:29 -0400

    Dia 3: Action and Menuitems

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -16,7 +16,8 @@
     'depends': ['base'],
     'data': [
         'security/academy_security.xml',
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/academy_menuitems.xml'
     ],
     'demo': [
         'demo/academy_demo.xml'
