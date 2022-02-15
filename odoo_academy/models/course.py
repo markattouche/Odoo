1a6778daa563c2f178b4ba4c8214bb3f5c2d280e
Adonis A. Veloz <a.veloz@anonymoussoft.onmicrosoft.com>
2022-02-15 14:47:22 -0400

    Replacing model name

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -3,7 +3,7 @@
 from odoo import models, fields, api
 
 class Course(models.Model):
-    _name = 'academy_course'
+    _name = 'academy.course'
     _description = 'Course Info'
     
     name = fields.Char(string='Title', required=True)
