0a1c27648b399f6f477b4d143a25e2a3184d357f
Adonis A. Veloz <a.veloz@anonymoussoft.onmicrosoft.com>
2022-02-16 16:07:58 -0400

    Dia 8: Herencias

---
+++ odoo_academy/models/sale_order.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class SaleOrder(model.Model):
+    _inherit = 'sale.order'
+    
+    session_id = fields.Many2one(
+        comodel_name='academy.session',
+        string='Related Session',
+        ondelete='set null'
+    )
+    
+    instructor = fields.Many2one(
+        string='Session Instructor',
+        related='session_id.instructor_id'
+    )
+    
+    student_ids = fields.Many2many(
+        string='Students',
+        related='session_id.student_ids'
+    )
+    
