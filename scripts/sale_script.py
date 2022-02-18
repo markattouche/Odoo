5bbe2ab61e3db017179be3564034679b41604420
Adonis A. Veloz <a.veloz@anonymoussoft.onmicrosoft.com>
2022-02-18 13:42:16 -0400

    Adding messegaes to translate

--- scripts/sale_script.py
+++ scripts/sale_script.py
@@ -1,12 +1,30 @@
-import xmlrpc.client
+from xmlrpc import client
 
 url = 'https://markattouche-odoo-develop-4264958.dev.odoo.com'
 db = 'markattouche-odoo-develop-4264958'
 username = 'admin'
 password = 'admin'
+# 3054e4c38f04c40fa0cf5a5c308eb2690f90bbb2
 
-common = xmlrpc.client.ServerProxy('%s/xmlrpc/2/common' % url)
+common = client.ServerProxy('%s/xmlrpc/2/common' % url)
 print(common.version())
 
 uid = common.authenticate(db, username, password, {})
 print(uid)
+
+models = client.ServerProxy('%s/xmlrpc/2/object' % url)
+
+model_access = models.execute_kw(db,uid,password, 'sale.order', 'check_access_rights',
+                                ['write'],{'raise_exception': False})
+
+print(model_access)
+
+draft_quotes = models.execute_kw(db, uid, password, 'sale.order', 'search',
+                                [[['state','=','draft']]])
+
+print(draft_quotes)
+
+if_confirmed= models.execute_kw(db,uid,password,
+    'sale.order','action_confirm',[draft_quotes])
+
+print(if_confirmed)
